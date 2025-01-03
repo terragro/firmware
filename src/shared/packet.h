1cfc12e76eacbf52130167e423aa321749b4352d
Codefoxdev <admin@codefoxdev.com>
2025-01-03 21:15:11 +0100

    Added the PacketHeader and PacketHeaderFlags classes

--- src/shared/packet.h
+++ src/shared/packet.h
@@ -1,7 +1,11 @@
+#include <array>
+
+using namespace std;
+
 #include <RadioLib.h>
 #include "LoRaBoards.h"
 
-typedef uint8_t Address;
+typedef uint16_t Address;
 
 class Packet
 {
@@ -12,25 +16,96 @@ public:
     // Creates a Packet instance from the raw packet
     static Packet from(String rawPacket);
 
-    static Packet toAddress(Address target, String payload)
-    {
-        Packet instance;
-        instance.target = target;
-        instance.payload = payload;
-        return instance;
-    }
-
-    static Packet toGateway(String payload)
-    {
-        return Packet::toAddress(0x00, payload);
-    }
-
     // instance side
-    uint8_t maxHops = 3; // The maximum amount of hops, defaults to 3, max of 4 bits
-    uint8_t hops = 0;    // The current amount of hops, max of 4 bits
-    Address target;      // The address of the receiving node
-    Address from;        // The address of the sender node
+    PacketHeader header;
     String payload;
+};
 
-    String encode();
+class PacketHeader
+{
+public:
+    // Creates a packet header to send to the specified address
+    static PacketHeader toAddress(Address destination, PacketHeaderFlags flags)
+    {
+        PacketHeader header;
+        header.destination = destination;
+        header.flags = flags;
+        return header;
+    }
+
+    // Creates a packet header to send to the gateway node (address: 0x0000)
+    static PacketHeader toGateway(PacketHeaderFlags flags)
+    {
+        return PacketHeader::toAddress(0x0000, flags);
+    }
+
+    Address destination;
+    Address sender;
+    uint32_t packetID;
+    PacketHeaderFlags flags;
+
+    // Use uint128?
+    // Returns a string with length 16, with all the header data packed in
+    String encode()
+    {
+        String str;
+
+        // Destination address
+        str[0] = destination & 0xff;
+        str[1] = (destination >> 8) & 0xff;
+
+        // Sender address
+        str[2] = sender & 0xff;
+        str[3] = (sender >> 8) & 0xff;
+
+        // Packet ID
+        str[4] = packetID & 0xff;
+        str[5] = (packetID >> 8) & 0xff;
+        str[6] = (packetID >> 16) & 0xff;
+        str[7] = (packetID >> 24) & 0xff;
+
+        // Flags
+        str[8] = flags.encode();
+
+        // Padding
+        str[9] = 0;
+        str[10] = 0;
+        str[11] = 0;
+        str[12] = 0;
+        str[13] = 0;
+        str[14] = 0;
+        str[15] = 0;
+
+        return str;
+    }
+};
+
+class PacketHeaderFlags
+{
+public:
+    static PacketHeaderFlags from(uint8_t raw)
+    {
+        PacketHeaderFlags flags;
+        flags.hops = raw & 0b111;
+        flags.maxHops = (raw >> 3) & 0b111;
+        flags.ack = (raw >> 6) & 0b1;
+        flags.rebroadcast = (raw >> 7) & 0b1;
+
+        return flags;
+    }
+
+    uint8_t hops = 0;           // The current amount of hops, max of 3 bits
+    uint8_t maxHops = 3;        // The maximum amount of hops, defaults to 3, max of 3 bits
+    boolean ack = true;         // If the receiver should send back an ACK
+    boolean rebroadcast = true; // If the message should be rebroadcasted by other nodes
+
+    uint8_t encode()
+    {
+        // Err if more than 3 bits?
+        hops &= 0b111;
+        maxHops &= 0b111;
+
+        uint8_t result = hops | (maxHops << 3) | (ack << 6) | (rebroadcast << 7);
+        return result;
+    }
 };
