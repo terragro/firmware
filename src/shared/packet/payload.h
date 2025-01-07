207b25df98bdd2202b2da14ca7491f7a8cce246d
Codefoxdev <admin@codefoxdev.com>
2025-01-07 21:41:38 +0100

    Added temporary MessagePayload

--- src/shared/packet/payload.h
+++ src/shared/packet/payload.h
@@ -5,19 +5,46 @@ namespace Packet
     enum PayloadType
     {
         ACK = 0,
+        MESSAGE = 1,
     };
 
     struct Payload
     {
         String encode();
+        PayloadType type;
     };
 
     struct ACKPayload : Payload
     {
+        static ACKPayload from(String raw)
+        {
+            ACKPayload payload;
+            return payload;
+        }
+
+        PayloadType type = ACK;
+
         String encode()
         {
             return emptyString;
         }
     };
 
+    struct MessagePayload : Payload
+    {
+        static MessagePayload from(String raw)
+        {
+            MessagePayload payload;
+            payload.data = raw;
+            return payload;
+        }
+
+        PayloadType type = MESSAGE;
+        String data;
+
+        String encode()
+        {
+            return data;
+        }
+    };
 }
