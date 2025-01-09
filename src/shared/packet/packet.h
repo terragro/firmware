c88691a7409da31ef485f20bf67f6e804468f80b
Codefoxdev <admin@codefoxdev.com>
2025-01-09 22:06:01 +0100

    Updated packet class to add status info

--- src/shared/packet/packet.h
+++ src/shared/packet/packet.h
@@ -1,9 +1,19 @@
 #include <RadioLib.h>
 #include <LoRaBoards.h>
-#include "header.h"
+#include "payload.h"
 
 namespace Packet
 {
+    enum STATE
+    {
+        PACKET_CREATED = 0,
+        PACKET_QUEUED = 1,
+        PACKET_TRANSMITTING = 2,
+        PACKET_TRANSMITTING_TIMEDOUT = 3,
+        PACKET_TRANSMITTING_FAILED = 4,
+        PACKET_TRANSMITTED = 5,
+    };
+
     class Packet
     {
     private:
@@ -11,29 +21,42 @@ namespace Packet
 
     public:
         // Creates a Packet instance from the raw packet
-        static Result<Packet, String> from(String rawPacket);
+        static Result<Packet, ERR_CODE> from(String raw)
+        {
+            if (raw.length() < 12)
+                return {Packet{}, ERR_INVALID_RAW_DATA};
 
-        // Static initializers
+            Header header = Header::from(raw);
+            Payload payload;
+            ERR_CODE err;
+            std::tie(payload, err) = parsePayload(header, raw.substring(12));
+            if (err != ERR_NONE)
+                return {Packet{}, err};
+
+            Packet packet(header, payload);
+            return {packet, ERR_NONE};
+        }
 
         // Creates a simple ACK message to `destination`
-        static Result<Packet, void *> ack(Address sender, Address destination)
+        static Result<Packet, ERR_CODE> ack(Address sender, Address destination)
         {
             HeaderFlags flags;
             flags.ack = false;
             ACKPayload payload;
 
-            Packet packet;
-            packet.header = Header::toAddress(sender, destination, flags, ACK);
-            packet.payload = payload;
-
-            return {packet, nullptr};
+            Header header = Header::toAddress(sender, destination, flags, ACK);
+            Packet packet(header, payload);
+            return {packet, ERR_NONE};
         }
+
+        Packet(Header header, Payload payload) : header(header), payload(payload) {}
 
         // instance side
 
         Header header;
         Payload payload;
         unsigned long timestamp;
+        STATE state = PACKET_CREATED;
 
         String encode()
         {
